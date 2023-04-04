6a8ee3830de252cbe592d9bd2cafebe90407a29d
Alexey Filonchikov <falex@au.ru>
2023-04-04 17:24:27 +0700

    fix: return to 5.5.0 sdk

--- lib/typescript/BannerView.d.ts
+++ lib/typescript/BannerView.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react" />
 import { StyleProp, ViewStyle } from 'react-native';
 type AdType = 'BANNER_300x250' | 'BANNER_300x300' | 'BANNER_320x50' | 'BANNER_320x100' | 'BANNER_240x400' | 'BANNER_728x90';
 interface BannerViewProps {
